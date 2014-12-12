553450952c1b495c8e63b743d21a4bacc04c3c42
wang-q <wang-q@outlook.com>
2014-12-12 16:32:30 +0800

    Simplify codes

--- weasel.pl
+++ weasel.pl
@@ -5,28 +5,23 @@ use Moose;
 with 'MooseX::Getopt';
 
 has 'size' => ( isa => 'Int', is => 'rw', default => 100, );
-has 'rate' => ( isa => 'Num', is => 'rw', default => 0.1, );
+has 'rate' => ( isa => 'Num', is => 'rw', default => 0.05, );
 has 'target' => (
     isa       => 'Str',
     is        => 'ro',
     default   => 'METHINKS IT IS LIKE A WEASEL',
     metaclass => 'NoGetopt',
 );
-has current_generation => (
+has 'current_generation' => (
     isa       => 'ArrayRef[Weasel]',
     is        => 'rw',
-    builder   => 'first_generation',
     metaclass => 'NoGetopt',
 );
 
-sub best {
-    my $self = shift;
-    $self->current_generation->[0];
-}
-
-sub first_generation {
+sub BUILD {
     my $self = shift;
 
+    # first generation
     my @population;
     for ( 1 .. $self->size ) {
         my $individual = Weasel->new(
@@ -35,8 +30,10 @@ sub first_generation {
         );
         push @population, $individual;
     }
+    $self->{current_generation}
+        = [ sort { $b->fitness <=> $a->fitness } @population ];
 
-    return [ sort { $b->fitness <=> $a->fitness } @population ];
+    return;
 }
 
 sub new_generation {
@@ -52,14 +49,14 @@ sub new_generation {
     $self->current_generation( [@population] );
 }
 
-sub run {
+sub best {
     my $self = shift;
-    $self->new_generation until $self->perfect_offspring;
+    $self->current_generation->[0];
 }
 
-sub perfect_offspring {
+sub run {
     my $self = shift;
-    return $self->best->perfect;
+    $self->new_generation until $self->best->perfect;
 }
 
 after new_generation => sub {
@@ -77,17 +74,16 @@ after run => sub {
 
 1;
 
-package Mutations;
+package Mutation;
 use Moose::Role;
-requires qw(mutate);
 
 use String::Compare;
 
-has fitness => ( isa => 'Num', is => 'rw', lazy_build => 1 );
+has 'fitness' => ( isa => 'Num', is => 'rw', lazy_build => 1 );
 
 sub _build_fitness {
     my $self = shift;
-    String::Compare::char_by_char( $self->string, $self->target );
+    return String::Compare::char_by_char( $self->string, $self->target );
 }
 
 sub inherit_string {
@@ -107,31 +103,13 @@ sub random_char {
     return ( 'A' .. 'Z', ' ' )[ rand(27) ];
 }
 
-1;
-
-package NonLockingMutations;
-use Moose::Role;
-with 'Mutations';
-
 sub mutate {
     my $self   = shift;
     my $idx    = shift;
     my $target = substr( $self->parent->string, $idx, 1 );
-    return $target unless rand() < $self->rate;
-    return $self->random_char;
-}
 
-1;
-
-package LockingMutations;
-use Moose::Role;
-with 'Mutations';
-
-sub mutate {
-    my $self   = shift;
-    my $idx    = shift;
-    my $target = substr( $self->parent->string, $idx, 1 );
-    return $target if $target eq substr( $self->target, $idx, 1 );
+    # locking to target
+    #return $target if $target eq substr( $self->target, $idx, 1 );
     return $target unless rand() < $self->rate;
     return $self->random_char;
 }
@@ -140,31 +118,34 @@ sub mutate {
 
 package Weasel;
 use Moose;
-with 'NonLockingMutations';
+with 'Mutation';
 
-has 'target'   => ( isa => 'Str',    is => 'ro', );
-has 'rate'     => ( isa => 'Num',    is => 'rw', );
-has parent     => ( isa => 'Weasel', is => 'ro', );
-has generation => ( isa => 'Int',    is => 'rw', builder => 'my_generation' );
+has 'target'     => ( isa => 'Str',    is => 'ro', );
+has 'rate'       => ( isa => 'Num',    is => 'ro', );
+has 'parent'     => ( isa => 'Weasel', is => 'ro', );
+has 'generation' => ( isa => 'Int',    is => 'ro', );
+has 'string'     => ( isa => 'Str',    is => 'ro', );
 
-sub my_generation {
+sub BUILD {
     my $self = shift;
-    return 0 unless $self->parent;
-    $self->parent->generation + 1;
-}
 
-has string => ( isa => 'Str', is => 'ro', lazy_build => 1 );
+    # build generation
+    if ( $self->parent ) {
+        $self->{generation} = $self->parent->generation + 1;
+    }
+    else {
+        $self->{generation} = 0;
+    }
 
-sub _build_string {
-    my $self = shift;
-    return $self->inherit_string if $self->parent;
-    return $self->random_str;
-}
+    # build string
+    if ( $self->parent ) {
+        $self->{string} = $self->inherit_string;
+    }
+    else {
+        $self->{string} = $self->random_str;
+    }
 
-sub _build_generation {
-    my $self = shift;
-    return 0 unless $self->parent;
-    $self->parent->generation + 1;
+    return;
 }
 
 sub perfect {
@@ -197,4 +178,5 @@ my $world = World->new_with_options;
 $world->run;
 
 __END__
->perl newweasels.pl --size 50 --rate 0.025
+
+>perl weasels.pl --size 50 --rate 0.025
